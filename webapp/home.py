ec096df354dc3768c4dbdf25c80327152c9aa403
Subin George <subingeorgemalana@gmail.com>
2021-07-11 17:53:48 +0530

    added menu and drawer
    Signed-off-by: Subin George <subingeorgemalana@gmail.com>

--- webapp/home.py
+++ webapp/home.py
@@ -3,14 +3,44 @@ import justpy as jp
 
 class Home:
 
-    path = "/home"
+    path = "/"
     @classmethod
     def serve(cls, request):
         wp = jp.QuasarPage()
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left", bordered=True)
+
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        q_list = jp.QList(a=scroller)
+        a_classes = "p-2 m-2 text-lg text-blue-400 hover:etxt-blue-700"
+        jp.A(a=q_list, text="Home", href="\home", classes=a_classes)
+        jp.Br(a=q_list)
+        jp.A(a=q_list, text="Dictionary", href="\dictionary", classes=a_classes)
+        jp.Br(a=q_list)
+        jp.A(a=q_list, text="About", href="\about", classes=a_classes)
+        jp.Br(a=q_list)
+
+
+
+
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen p-2")
         jp.Div(a=div, text="This is the Home page!", classes="text-4xl m-2")
         jp.Div(a=div, text="""
         poinddgonoasdngoh  obssdgb F BUJSDg knodbf OFPIWrijWEFI WEIBOWBE Oongnoe
         """, classes="text-lg")
         return wp
 
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
